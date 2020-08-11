24dac546ea26678e4a3f8fe13917e60abf57d326
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2020-08-11 19:03:41 -0400

    Update static pages

--- pages/faq.vue
+++ pages/faq.vue
@@ -3,75 +3,102 @@
     <div class="content">
       <h1>FREQUENTLY ASKED QUESTIONS ON {{ $siteConfig.siteName }}</h1>
       <p>
-        Whether from friends, colleagues, or from the web, here are a few
-        frequently asked questions about our film site.
+        Whether from friends, colleagues, or the web, here are a few frequently
+        asked questions about our film site.
       </p>
       <h3>
-        How does {{ $siteConfig.siteName }} go about choosing the films it will
-        comment on?
+        Q. How does The Movie Shrink go about choosing the films it will comment
+        on?
       </h3>
       <p>
-        There are some accidental elements to choosing films we will comment on.
-        The films are chosen among the ones that are accessible to us in our own
-        city, or on the web, or then again in a film festival we attend. There
-        is no pretense to say that the films chosen are the most significant
-        current films. But they are part of them. It is taken for granted that
-        if we choose to comment on a film, it is that there is social meaning to
-        it, usually implicit, a meaning we try to uncover and understand. But
-        the meaning we will try to understand will not be going in all
-        directions, there will have to be some common thread, some unity to it.
-        Most often, the meaning we will uncover will be related to the elements
-        of understanding we presented in our section on the on the Foundations
-        of {{ $siteConfig.siteName }}. The sources mentioned there can be
+        A. There are some accidental elements to choosing films we will comment
+        on, although great care is taken in choosing our two films per month,
+        our twenty-four films a year. We view several more films on which we
+        choose not to comment on in detail. Some of them are briefly commented
+        upon in our
+        <nuxt-link to="/categories/bullit-comments">Bullet comments</nuxt-link>
+        section of this site.
+      </p>
+      <p>
+        The films that are looked at closely in our main section are chosen
+        among the ones that are accessible to us in our city, or on the web, or
+        then again, in a film festival we attend. There is no pretense to say
+        that the films chosen are the most significant current films. But they
+        are part of them. Of course, a great many significant films, in fact,
+        most of them, do not find their way into The Movie Shrink. There are
+        more than twenty-four significant films a year on the planet, of course.
+      </p>
+      <p>
+        It is taken for granted that if we choose to comment on a film, it is
+        that there is social meaning to it, usually implicit, meaning we try to
+        uncover and understand. But the meaning we will try to understand will
+        not be going in all directions, there will have to be some common
+        thread, some unity to it. Most often, the meaning we will uncover will
+        be related to the elements of understanding we presented in our section
+        on The Movie Shrink’s toolbox. The sources mentioned there can be
         considered as our keys to understanding films, in this site.
+        Interpretive sociology, Max Weber, Economic institutionalism, and
+        Marshall McLuhan are, so to speak, the basic ingredients behind The
+        Movie Shrink’s interpretations, along with some others.
       </p>
       <h3>
-        How does {{ $siteConfig.siteName }} go about in identifying what it
-        considers as meaningful elements within a given film?
+        Q. How does The Movie Shrink go about in identifying what it considers
+        as meaningful elements within a given film?
       </h3>
       <p>
-        Although we try to be as explicit and systematic as possible, the
-        procedure is not really amenable to the standard procedures of
-        scientific inquiry. At the outset, let us recognize that the films are
-        not not chosen randomly, as stated earlier. There is a certain amount of
-        intuition and experience involved. However, once the interpretation is
-        presented, it must be defended and justified in the most objective way
-        possible, it must be able to withstand criticism and contradictory
-        opinions, much like in a court procedure. The essential goal is the
-        identification of a central theme, expressed in many ways. This central
-        theme is at the heart of what is looked for. If this central theme seems
-        absent, there is no need for us to go further. In the cases we “come up
-        empty” with, that is to say in most cases of the films we look at before
-        making a choice, the film can have meaning and be socially relevant,
-        even profound, but the hermeneutic method will not yield more than what
-        can be gathered from casual observations. In some cases, the film
-        director is very conscious of what he wants to say, to the point that
-        his film is an advocacy for some cause, often political, as in the
-        Costa-Gavras films of the 20th century, intent on revealing governmental
-        corruption in South America and other parts of the world. The
-        hermeneutic method, as we understand it, cannot add much in those cases,
-        as with the documentary approach, where the opinion of the film maker is
-        paramount and in a sense self-sufficient.
+        A. Although we try to be as explicit and systematic as possible, the
+        procedure is not amenable to the standard procedures of scientific
+        inquiry. At the outset, let us recognize that the films are not chosen
+        randomly, as stated earlier. There is a certain amount of intuition and
+        experience involved. However, once the interpretation is presented, it
+        must be defended and justified in the most objective way possible, it
+        must be able to withstand criticism and contradictory opinions, much
+        like in a court procedure.
       </p>
-      <h3>Are there not as many interpretations as there are viewers?</h3>
       <p>
-        In In the view taken here, there is room for different interpretations,
+        The essential goal is the identification of a central theme, expressed
+        in many ways.
+      </p>
+      <p>
+        This central theme is at the heart of what is looked for. If this
+        central theme seems absent, there is no need for us to go further. In
+        the cases we “come up empty” with, that is to say in most cases of the
+        films we look at before making a choice, the film can have meaning and
+        be socially relevant, even profound, but the hermeneutic method will not
+        yield more than what can be gathered from casual observations. In some
+        cases, the film director is very conscious of what he wants to say, to
+        the point that his film is advocacy for some cause, often political, as
+        in the Costa-Gavras films of the 20th century, intent on revealing
+        governmental corruption in South America and other parts of the world.
+        The hermeneutic method, as we understand it, cannot add much in those
+        cases, as with the documentary approach, where the opinion of the
+        filmmaker is paramount and in a sense self-sufficient.
+      </p>
+
+      <h3>
+        Q. Are there not as many interpretations as there are viewers?
+      </h3>
+      <p>
+        A. In the view taken here, there is room for different interpretations,
         and these interpretations, although different, are not necessarily in
-        opposition to each other. In our interactive site (XXXXXXX) there will
-        be ample space for other interpretations, other elements of
-        understanding. However, not all interpretations are equal. There are
-        some interpretations which are, in the words of hermeneutics, « more
-        adequate to the object ». Some interpretations are more enlightening. We
-        are not, as suggested earlier, in a scientific scientific procedure, but
-        that is not to conclude that everything is relative. There is the
-        necessity to demonstrate the appropriateness of the proposed
-        understanding and, as stated earlier, to defend it, much like in a
-        trial, where absolute certainty cannot always be attained, and where,
-        yet, a decision is made. We will add other elements to this “More on
-        hermeneutics” section in later entries, but these opening comments
-        constitute more information on the basic elements of our approach. And
-        now, let us turn to some specific films ! Jean-Charles Mercier
-        {{ $siteConfig.siteName }} June 2, 2019.
+        opposition to each other. In our interactive site (The Movie Shrinks
+        Forum) there will be ample space for other interpretations, other
+        elements of understanding.
+      </p>
+      <p>
+        However, not all interpretations are equal. There are some
+        interpretations which are, in the words of hermeneutics, « more adequate
+        to the object » or “more sensitive to the object”. Some interpretations
+        are more enlightening. We are not, as suggested earlier, in a scientific
+        procedure, but that is not to conclude that everything is relative.
+        There is the necessity to demonstrate the appropriateness of the
+        proposed understanding and, as stated earlier, to defend it, much like
+        in a trial, where absolute certainty cannot always be attained, and
+        where, yet, a decision is made.
+      </p>
+
+      <p>
+        And now, let us turn to some specific films!
       </p>
     </div>
   </div>
--- pages/hermeneutics.vue
+++ pages/hermeneutics.vue
@@ -1,43 +1,32 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <div class="content">
-      <h3>A FEW WORDS ON HERMENEUTICS</h3>
+      <h2>MORE ON HERMENEUTICS</h2>
       <p>
-        Hermeneutics is involved in the study and art of interpretation. The
-        origins of the word “hermeneutic” is somewhat disputed, and sometimes
+        Hermeneutics is involved in the study and art of interpreting. The
+        origins of the word “hermeneutic” are somewhat disputed, and sometimes
         its origins are associated with the role of Hermes, the Greek messenger
         of the gods, bringing messages to humans.
       </p>
       <p>
-        Hermeneutics were at the very outset, and still are today, concerned
+        Hermeneutics has been concerned at the very outset, and still are today,
         with the deciphering of challenging or unclear texts.
       </p>
       <p>
-        And so, at first, hermeneutics were involved, above all and almost
+        And so, at first, hermeneutics has been involved, above all and almost
         exclusively, with written texts, most often religious or legal texts.
         But French hermeneutic philosopher Paul Ricoeur extended hermeneutics’
         domain to “text like” creations or human productions, which includes,
-        for example, architecture, paintings and most of the artistic
+        for example, architecture, paintings, and most of the artistic
         expressions, including, in our case, films and cinema.
       </p>
-
-      <h3>MORE ON HERMENEUTICS</h3>
       <p>
-        In this section, we want to move deeper into understanding films through
-        the use of hermeneutics.
+        Now let us move deeper into understanding films through the use of
+        hermeneutics.
       </p>
       <p>
-        Stated broadly, hermeneutics concerns the art and method of
-        interpretation, as we stated earlier. It has of course many dimensions
-        and applications. Its application to films is part of the larger
-        tradition of applying hermeneutics to artistic creations. Seen in this
-        light, art is not only a question of aesthetics, but it participates in
-        the search for knowledge. More specifically, in our case, knowledge
-        about society and its many contemporary dimensions.
-      </p>
-      <p>
-        Let us now look at how the need to better understand challenging and
-        significant films leads us to hermeneutics.
+        Let us look, more specifically, at how the need to better understand
+        challenging and significant films leads us to hermeneutics.
       </p>
       <p>
         In a sense, all films speak about the human story but, on this site, we
@@ -51,10 +40,10 @@
         immediate understanding. The story that is told in the film has some
         quite explicit aspects, while, at the same time, leaving some important
         elements understated or implicit. These implicit but important aspects
-        become, as it were, the elephant behind the screen, and they are not as
-        much hidden as they are in need of interpretation, in need of additional
-        work for the spectator. Such is the hermeneutic approach in
-        {{ $siteConfig.siteName }}.
+        become, as it were, the Movie Shrink’s basic material, and they are not
+        as much hidden as they are in need of interpretation, in need of
+        additional work for the spectator. Such is the hermeneutic approach to
+        understanding significant films.
       </p>
       <p>
         The additional work required for the spectator, or the analyst, is often
@@ -64,29 +53,27 @@
         unrelated. Finding a significant unity in certain films, beyond their
         first reading, is the task we want to respond to in this film site.
         Then, an effort is made to tie this understated meaning to contemporary
-        society and to the human story, as it unfolds in the twenty first
-        century.
+        society and the human story, as it unfolds in the twenty-first century.
       </p>
-      <h3>FOUNDATIONS FOR FILMS FOR THOUGHT</h3>
-      <p>
-        Let us come back to hermeneutics, the study and art of interpretation,
-        as the main basis of the commentaries we offer on films on this site.
-      </p>
+
+      <h3>
+        THE MOVIE SHRINK’S TOOLBOX
+      </h3>
       <p>
         While being attentive to the risk of too much interpretation, taken too
         far, we nevertheless think that artistic creation has something to say
         to us about the human experience, beyond its aesthetic dimension, as
         suggested earlier. German philosopher Georg H. Gadamer, a specialist on
         hermeneutics, had criticized in his long and distinguished career the
-        tendency to see art as only a question of aesthetics. Art is indeed,
-        according to Gadamer, also a source of knowledge about society, more
-        specifically about the contemporary human experience. As artist, the
-        filmmaker himself does not always plan to present this knowledge
-        deliberately, as we suggested earlier. That is not a problem, quite the
-        opposite, because there is a certain degree of unconsciousness in all
-        artistic creation. The role of the hermeneutic process, on the other
-        hand, is to bring to a greater level of consciousness this meaning when
-        this meaning is difficult to identify.
+        tendency to see art as only a question of aesthetics. Art is, according
+        to Gadamer, also a source of knowledge about society, more specifically
+        about the contemporary human experience. As an artist, the filmmaker
+        himself does not always plan to present this knowledge deliberately, as
+        we suggested earlier. That is not a problem, quite the opposite, because
+        there is a certain degree of unconsciousness in all artistic creation.
+        The role of the hermeneutic process, on the other hand, is to bring to a
+        greater level of consciousness this meaning when this meaning is
+        difficult to identify.
       </p>
       <p>
         There is no need for the application of the hermeneutic method when, at
@@ -96,11 +83,11 @@
         under study.
       </p>
       <p>
-        There are other foundational elements which inspire our site,
-        Understanding Significant Film.
+        There are other foundational elements that make up the Movie Shrink’s
+        toolbox.
       </p>
       <p>
-        We referred earlier to the contribution of French hermeneutics
+        We referred earlier to the contribution of French hermeneutic
         philosopher Paul Ricoeur, and specifically to his suggestion that we
         include in the hermeneutic work “text like” creations, which opened the
         door to consider films and cinema as objects of hermeneutic
@@ -112,8 +99,8 @@
       </p>
       <p>
         It concerns Ricoeurs’s idea that, when interpreting a text or a text
-        like object under scrutiny, we often do not start with a completely
-        clean slate. Sometimes, maybe most of the times, we have an idea of what
+        like element under scrutiny, we often do not start with a completely
+        clean slate. Sometimes, maybe most of the time, we have an idea of what
         we are trying to find, a sort of preliminary understanding of what we
         are looking for. He calls this preunderstanding the hermeneutics of
         “suspicion”, as opposed to the case when we look at something with a
@@ -123,71 +110,76 @@
         The hermeneutics of suspicion, as a starting point, is neither worse nor
         better than the hermeneutics of openness, they are only different. The
         preunderstanding of the hermeneutics of suspicion is not necessarily
-        problematic, since our preliminary understanding can be useful if kept
-        in check and abandoned when it proves false or misleading. Many other
+        problematic since our preliminary understanding can be useful if kept in
+        check and abandoned when it proves false or misleading. Many other
         hermeneutic philosophers have given attention to this process of
-        preunderstanding, and Gadamer and his own teacher, Martin Heidegger,
-        have pointed out that this process is unavoidable and must be
-        recognized.
+        preunderstanding, and Gadamer and his teacher, Martin Heidegger, have
+        pointed out that this process is unavoidable and must be recognized.
       </p>
       <p>
-        In this spirit, we want to make explicit some the basic elements that
+        In this spirit, we want to make explicit some of the basic elements that
         constitute our own preunderstanding of the films we look at in this
-        commentary site on cinema.
+        commentary site on cinema. These elements are, as it were, The Movie
+        Shrink’s toolbox.
       </p>
       <p>
         First, it must be said that the author of this site comes from what is
         called the humanities and the social sciences, more specifically from
-        political science, sociology and economics. In the preunderstanding of
+        political science, sociology, and economics. In the preunderstanding of
         the films we choose to look at, we indeed believe that the film cannot
-        escape the context or the times in which it is created. It can only
+        escape the context of the times in which it is created. It can only
         reflect the times that give birth to its creation, whether the film
         portrays the present, the past or the future. More specifically, a film
         reveals something of the socio-economic context in which it appears. All
         through the twentieth century, and beyond, the history of cinema,
         starting at the same time as the twentieth century itself, was witness
         to all manner of historic, social and economic changes, reflecting,
-        among other changes, the greater reach of our communication medias and
+        among other changes, the greater reach of our communication media and
         the growing size and scale of human organizations, from village, to
-        growing city, to nations, and to globalization, and possibly moving to
-        smaller scale again.
+        growing city, to nations, and globalization, and possibly moving to
+        smaller scale again. These elements are large scale and moving slowly.
+        They are, in a sense, the elephants behind the screen. We can sometimes
+        get a glimpse of these movements in works of art or films.
       </p>
       <p>
         When looking at the films we choose to spend time on, which are not
-        chosen randomly of course, we are mindful of these socio-economic
+        chosen randomly, of course, we are mindful of these socio-economic
         dynamics, not to judge them or take positions on them, but only to
         better describe them and understand them.
       </p>
       <p>
-        Ideally, the story that the films tells is related to the evolving human
+        Ideally, the story that the film tells is related to the evolving human
         story of where we are, as humans, at this time in history. Films can be
-        seen as chapters in a series about our collective human story.
+        seen as chapters in the human story.
       </p>
       <p>
         In trying to understand these socio-economic and historic aspects, we
         rely on some specific disciplines that, together, make up the elements
-        of our preunderstanding and our own hermeneutics of suspicion.
+        of our preunderstanding and our hermeneutics of suspicion.
       </p>
       <p>
         Drawing from sociology, we are guided by some aspect of its
         interpretative (or interpretive) and historical tradition, found among
         others in the work of Max Weber. In economics, we are mostly interested
-        in Economic Institutionalism, a school very much interested in the
-        changes brought by evolving patterns of human and economic interactions.
-        Finally, from a media history point of view, the work of Canadian media
-        philosopher Marshall McLuhan (« the global village » and « the medium is
-        the message ») is for us an important source of understanding.
+        in Economic institutionalism (sometimes referred to as New Economic
+        Institutionalism) a school very much interested in the changes brought
+        by evolving patterns of human and economic interactions. Finally, from a
+        media history point of view, the work of Canadian media philosopher
+        Marshall McLuhan (« the global village » and « the medium is the message
+        ») is for us an important source of understanding.
       </p>
       <p>
-        As stated earlier, it is in keeping with many hermeneutic analyses to
-        recognize that there is a kind of pre-understanding, possibly abandoned
-        later, in any effort at understanding. We have just exposed some
-        elements of our own pre-understanding in referring to interpretative
-        sociology, institutional economics and McLuhan theories on the history
+        These elements and authors can be seen as constituting an important part
+        of The Movie Shrink’s toolbox, in addition to hermeneutics. As stated
+        earlier, it is in keeping with many hermeneutic analyses to recognize
+        that there is a kind of pre-understanding, possibly abandoned later, in
+        any effort at understanding. We have just exposed some elements of our
+        pre-understanding in referring to Max Weber, to interpretative
+        sociology, institutional economics, and McLuhan theories on the history
         of media.
       </p>
       <p>
-        There are other sources for understanding films in this site, but the
+        There are other sources for understanding films on this site, but the
         ones just mentioned can be considered as the principal ones.
       </p>
     </div>
