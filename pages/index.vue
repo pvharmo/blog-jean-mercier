926fef817fe31876c79ac5d8c66f932a9734d1d3
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2020-09-04 13:59:13 -0400

    Hide chronicles from home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
-    <posts-grid :posts="$store.state.posts" />
+    <posts-grid :posts="posts" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
+  computed: {
+    posts() {
+      const filteredPosts = this.$store.state.posts.filter((x) => {
+        return !(
+          x.category.includes('Bullet comments') ||
+          x.category.includes('Revisiting classics') ||
+          x.category.includes('More on hermeneutics')
+        )
+      })
+      return filteredPosts
+    }
+  },
   head() {
     return {
       title: `Home | ${this.$siteConfig.siteName}`
