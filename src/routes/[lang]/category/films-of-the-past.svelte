744154200bbf50ca1c2f03eb70e10bdf05de0acb
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2021-01-31 19:55:30 -0500

    Add chronicles post list page

---
+++ src/routes/[lang]/category/films-of-the-past.svelte
@@ -0,0 +1,32 @@
+<script context="module">
+    export async function preload({ params: {lang} }) {
+      await fetchCategories(this, lang);
+      await fetchTags(this, lang);
+    }
+  </script>
+  
+  <script>
+    import { stores } from "@sapper/app";
+    import t from "../../../locales/language";
+  
+    import { revisitingClassics } from "../../../stores";
+    import { fetchCategories, fetchTags } from "../../../actions";
+    import { getLang } from "../../../utils";
+  
+    import PostsGrid from "../../../components/PostsGrid.svelte";
+  
+    const { page } = stores();
+  
+    $: filteredPosts = $revisitingClassics
+  
+    $: lang = getLang($page.path);
+  </script>
+  
+  <svelte:head>
+    <title>{t(lang).sidebar["Revisiting classics"]}</title>
+  </svelte:head>
+  
+  <div id="posts-by-category-page" class="page-wrapper home-page">
+    <h1 class="title" style="text-align: center">{t(lang).sidebar["Revisiting classics"]}</h1>
+    <PostsGrid posts={filteredPosts} {lang} />
+  </div>
