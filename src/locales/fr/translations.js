f230a22c53ad0ecac348d0315a9e0ce29d9e018e
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2021-02-04 14:01:56 -0500

    Adjust translations

--- src/locales/fr/translations.js
+++ src/locales/fr/translations.js
@@ -5,20 +5,20 @@ export default {
     about: "À propos",
     tagline: "Comprendre nos films, comprendre notre époque",
     sidebar: {
-        intro: "is attempting to intepret and undestand complex films, just as Aristotle, laying his hands on a bust of Homer, is trying to get an understanding of the poet's texts in this Rembrandt painting.",
+        intro: "veut comprendre des films particulièrement exigeants, comme Aristote , en posant les mains sur le buste de Homère, essaie de comprendre les textes du poète, dans cette peinture de Rembrandt",
         Chronicles: "Chroniques",
         "Bullet comments": "Commentaires éclair",
-        "Revisiting classics": "Classiques revisités",
-        "More on hermeneutics" : "Plus sur l'herméneutique",
-        "The Shrink's Toolbox": "La boîte à outils du Shrink",
+        "Revisiting classics": "Des classiques revisités",
+        "More on hermeneutics" : "En savoir plus sur l'herméneutique",
+        "The Shrink's Toolbox": "La boîte à outils du Movie Shrink",
         Region: "Région"
     },
     menu: {
         HOME: "ACCUEIL",
-        "ABOUT US": "À PROPOS",
+        "ABOUT US": "À PROPOS DE NOUS",
         HERMENEUTICS: "HERMÉNEUTIQUE",
-        FAQ: "FAQ",
-        CONTACT: "CONTACT"
+        FAQ: "QUESTIONS-RÉPONSES",
+        CONTACT: "NOUS CONTACTER"
     },
-    readMore: "Lire la suite"
+    readMore: "Pour en savoir plus"
 }
