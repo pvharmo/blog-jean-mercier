2ff1ae9368cc0f60f1bc08c5b11889f90401cfe1
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2022-09-24 17:34:56 -0400

    Add layout for articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+function defaultLayoutPlugin () {
+    return function (tree, file) {
+        file.data.astro.frontmatter.layout = '@layouts/Post.astro'
+    }
+}
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+        remarkPlugins: [defaultLayoutPlugin]
+    },
+    build: {
+        format: 'file'
+    }
+});
