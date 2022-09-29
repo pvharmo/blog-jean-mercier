6eadd7dae1670961ad0d0625c6b18e4edf96fa85
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2022-09-29 10:03:54 -0400

    add sitemap and compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,32 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
 
-function defaultLayoutPlugin () {
-    return function (tree, file) {
-        file.data.astro.frontmatter.layout = '@layouts/Post.astro'
-    }
+function defaultLayoutPlugin() {
+  return function (tree, file) {
+    file.data.astro.frontmatter.layout = '@layouts/Post.astro';
+  };
 }
+
+
 
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        remarkPlugins: [defaultLayoutPlugin]
-    },
-    build: {
-        format: 'file'
-    }
+  site: "https://themovieshrink.com/",
+  markdown: {
+    remarkPlugins: [defaultLayoutPlugin]
+  },
+  build: {
+    format: 'file'
+  },
+  integrations: [compress(),
+    sitemap({  
+      i18n: {
+        defaultLocale: 'en',   // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en-CA',         // The `defaultLocale` value must present in `locales` keys
+          fr: 'fr-CA',
+        },
+      }
+    })]
 });
